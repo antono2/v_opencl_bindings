bc949e2946dd717e6a31eadb07ea7d7d1e7dcbfd
Anton Oreskin <antono2@web.de>
2026-09-06 15:04:08 +0200

    Allow optional OpenCL 2.1 timers on 3.0 devices

--- test/main.c.v
+++ test/main.c.v
@@ -20,9 +20,14 @@ fn main() {
 	check(cl.get_device_ids(platform, cl.device_type_all, 1, &device, unsafe { nil }), 'get device')
 	mut device_timestamp := u64(0)
 	mut host_timestamp := u64(0)
-	check(cl.get_device_and_host_timer(device, &device_timestamp, &host_timestamp), 'get device and host timer')
-	assert device_timestamp > 0
-	assert host_timestamp > 0
+	timer_result := cl.get_device_and_host_timer(device, &device_timestamp, &host_timestamp)
+	if timer_result == cl.success {
+		assert device_timestamp > 0
+		assert host_timestamp > 0
+	} else {
+		// OpenCL 3.0 makes formerly mandatory 2.x capabilities optional.
+		assert timer_result == cl.invalid_operation
+	}
 
 	mut error_code := cl.success
 	context := cl.create_context(unsafe { nil }, 1, &device, unsafe { nil }, unsafe { nil }, &error_code)
